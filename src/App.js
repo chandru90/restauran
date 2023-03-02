1a734828fe41bce9799e813e94f6e1e862bd2f10
chandru90 <iochandru90@gmail.com>
2023-03-02 18:24:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React , {useState} from 'react'
 import './App.css';
+import RestForm from './components/RestForm'
+import ItemList from './components/ItemList'
 
 function App() {
+  const [userslist,setuserlist]  = useState('')
+const adduserhandler =(uid,udish) =>{
+  setuserlist(prevuserlist => {
+    return [...prevuserlist,{id :uid , dish: udish}]
+  }
+  )
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RestForm onrestform = {adduserhandler}></RestForm>
+      <ItemList users ={userslist} ></ItemList>
     </div>
+    
   );
 }
 
